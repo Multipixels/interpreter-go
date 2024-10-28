d91daa434cf13356afeae2b27f35b92de908ad98
Richard <75509898+Multipixels@users.noreply.github.com>
2024-10-27 23:53:36 -0400

    finish ch1

---
+++ monkey/token/token.go
@@ -0,0 +1,70 @@
+package token
+
+// type aliasing in Go
+// not performant using strings, but it'll be easier for debugging reasons (and learning)
+type TokenType string
+
+// List of constants
+const (
+	ILLEGAL = "ILLEGAL" // Non-expected character
+	EOF     = "EOF"     // end of file
+
+	// Identifiers and Literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN  = "="
+	PLUS    = "+"
+	MINUS   = "-"
+	BANG    = "!"
+	ASTERIX = "*"
+	SLASH   = "/"
+
+	LT     = "<"
+	GT     = ">"
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+// define Token as a struct of TokenType "Type", and string "Literal"
+type Token struct {
+	// Defined as variable name, followed by type
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
---
+++ timeline.md
@@ -0,0 +1,14 @@
+# Timeline
+
+## 2024/10/21
+- Start following along with the textbook.
+- Set up Go on machine.
+
+## 2024/10/22
+- Ch 1.1-1.2
+- Create token.go
+
+## 2024/10/23
+- Ch 1.3-1.5
+- Create our tokenizer and extended it to the full Monkey token list
+- Start creating our REPL (without the E)
